cc22084fb9a39e3196b10493d92031fd1054279b
christopher lopez <christopherwlopez89@gmail.com>
2023-04-05 21:12:40 -0700

    scaffolding and creating store

---
+++ src/Components/Categories/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import { ButtonGroup } from "@mui/material";
+
+function Categories() {
+  return (
+  <>
+    <ButtonGroup>
+      <Button>Electronics</Button>
+      <Button>Food</Button>
+      <Button>Clothing</Button>
+    </ButtonGroup>
+  </>  
+   
+  )
+}
+
+export default Categories;
---
+++ src/Components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; Lopez</p>
+    </footer>
+  )
+}
+
+export default Footer
