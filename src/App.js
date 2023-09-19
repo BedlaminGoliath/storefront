aea1f72d776eade6362ccf6f48527ff8cf9e4152
christopher lopez <christopherwlopez89@gmail.com>
2023-09-18 18:29:52 -0700

    app changes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import React from 'react';
 import Header from "./Components/Header";
 import Categories from "./Components/Categories";
 import Footer from "./Components/Footer";
-import { BrowserRouter, Route, Routes} from "react-router-dom";
-import  Storefront  from "./Components/StoreFront";
-import Error from "./Components/Error";
-import Layout from "./Components/Layout";
-import ProductDetails from "./Components/ProductDetails";
 const App =()=> {
   return (
     
