ee393e5c4b946cf60f296f00d913260fea120857
christopher lopez <christopherwlopez89@gmail.com>
2023-09-18 18:24:26 -0700

    fixed bug in category component

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import Footer from "./Components/Footer";
  function App() {
   return (
     
-      <div>
+    <>
       <Header/>
       <Categories/>
       <Footer/>
-    </div>
+    </>
   )
   
 }
--- src/Components/Categories/index.js
+++ src/Components/Categories/index.js
@@ -2,23 +2,21 @@ import React from "react";
 import Products from "../Products";
 import { useSelector } from "react-redux";
 import { filteredProducts } from "../../store/products";
-import Grid from "@mui/material/Grid"
+import Grid from "@mui/material/Unstable_Grid2"
 
 function Categories() {
   const products = useSelector(filteredProducts);
   return(
     <>
-    <Grid container direction="row" justifycontent="space-around" alignItems="Center">
-      {products.map((products)=> {
-        <Grid xs={5}>
-        <Products product={products}/>
-      </Grid>
-      })}
+    <Grid container spacing="row" justifycontent="space-around" alignItems="Center">
+          {products.map((category)=> (
+    <Grid key={category.name}xs={4}>
+          <Products product={category}/>
+    </Grid>
+      ))}
     </Grid>
       </>
-  )
-
-  
+  );
 }
 
 export default Categories;
