99d6a5663a35c6cdd96c6204feae7acc2f09ae63
christopher lopez <christopherwlopez89@gmail.com>
2023-09-18 18:46:08 -0700

    header component

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,10 +1,47 @@
 import React from 'react'
+import  AppBar  from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import { useDispatch } from 'react-redux';
+import { Container, Toolbar, Typography } from '@mui/material';
+import categories from '../../store/categories';
+import productSlice from '../../store/products';
 
 function Header() {
+  
+  const dispatch =useDispatch();
+
   return (
-    <>
-      <h2>The Store</h2>
-    </>
+    <AppBar>
+      <Container>
+        <Toolbar>
+          <Typography
+            role='banner'
+            variant='h4'
+            noWrap
+            component='a'
+            href='/'
+          >
+            <h2>The Squirrel Store!</h2>
+          </Typography>
+          <Box>
+            {categories.map((category)=> (
+              <Button
+                key={category.name}
+                onClick={()=>{
+                  dispatch(
+                    productSlice.actions.setActiveCategory(category.name)
+                  );
+                }}
+              >
+                {category.displayName}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+
   )
 }
 
