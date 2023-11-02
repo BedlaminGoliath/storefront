90c88333c9c1a869904ac5c16c8571d413b96804
christopher lopez <christopherwlopez89@gmail.com>
2023-11-01 18:03:36 -0700

    adding the decrease, increase reducers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from 'react';
 import Header from "./Components/Header";
 import Categories from "./Components/Categories";
 import Footer from "./Components/Footer";
+
 const App =()=> {
   return (
     
     <>
-      <Header/>
-      <Categories/>
-      <Footer/>
+      <BrowserRouter>
+        <Routes>
+          <Route>
+            <Header/>
+            <Categories/>
+            <Footer/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
   
